63258eff730eaffee6deba0e20c00aba2935879d
YangYunteng <18302010022@fudan.edu.cn>
2021-04-17 22:46:26 +0800

    初步配置

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    host: 'localhost',
+    port: 80,
+    // 以下代码会使项目运行成功后自动打开浏览器
+    // open: true
+  },
+};
